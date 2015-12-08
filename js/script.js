da8c6549281924169a12138bfa44e28090c6e9e7
liyan90s <liyan1990s@gmail.com>
2015-12-07 22:41:05 -0800

    enable prev and next buttons

--- js/script.js
+++ js/script.js
@@ -61,8 +61,99 @@ function search() {
 				// display results
 				$('#results').append(output);
 			});
+
+			var buttons = getButtons(prevPageToken, nextPageToken);
+
+			// display buttons
+			$('#buttons').append(buttons);
 		}
 	);
+}
+
+// Next Page Function 
+function nextPage() {
+	var token = $('#next-button').data('token');
+	var q = $('#next-button').data('query');
+
+	// var  results
+	$('#results').html('');
+	$('#buttons').html('');
+
+	// get Form input
+	q = $('#query').val();
+
+	// run GET request on API
+	var url = 'https://www.googleapis.com/youtube/v3/search';
+	$.get(url, {
+		part: 'snippet, id',
+		q: q,
+		pageToken: token,		// get next page !
+		type: 'video',
+		key: 'AIzaSyCM7-zaWMB9ZQ5bQGQmkPAVirUNjX1WQDY'},
+		function(data) {
+			var nextPageToken = data.nextPageToken;
+			var prevPageToken = data.prevPageToken;
+
+			// log data
+			console.log(data);
+
+			$.each(data.items, function(i, item) {
+				// get output
+				var output = getOutput(item);
+
+				// display results
+				$('#results').append(output);
+			});
+
+			var buttons = getButtons(prevPageToken, nextPageToken);
+
+			// display buttons
+			$('#buttons').append(buttons);
+		}
+	);	
+}
+
+// Prev Page Function 
+function prevPage() {
+	var token = $('#prev-button').data('token');
+	var q = $('#prev-button').data('query');
+
+	// var  results
+	$('#results').html('');
+	$('#buttons').html('');
+
+	// get Form input
+	q = $('#query').val();
+
+	// run GET request on API
+	var url = 'https://www.googleapis.com/youtube/v3/search';
+	$.get(url, {
+		part: 'snippet, id',
+		q: q,
+		pageToken: token,		// get next page !
+		type: 'video',
+		key: 'AIzaSyCM7-zaWMB9ZQ5bQGQmkPAVirUNjX1WQDY'},
+		function(data) {
+			var nextPageToken = data.nextPageToken;
+			var prevPageToken = data.prevPageToken;
+
+			// log data
+			console.log(data);
+
+			$.each(data.items, function(i, item) {
+				// get output
+				var output = getOutput(item);
+
+				// display results
+				$('#results').append(output);
+			});
+
+			var buttons = getButtons(prevPageToken, nextPageToken);
+
+			// display buttons
+			$('#buttons').append(buttons);
+		}
+	);	
 }
 
 // Build output
@@ -89,4 +180,20 @@ function getOutput(item) {
 	'';
 
 	return output;
+}
+
+// Build the buttons
+function getButtons(prevPageToken, nextPageToken) {
+	if (!prevPageToken) {
+		var btnoutput = '<div class="button-container">'+'<button id="next-button" class="paging-button" data-token="'+nextPageToken+'" data-query="'+q+'"' +
+		'onclick="nextPage();">Next Page</button></div>';
+	} else {
+		var btnoutput = '<div class="button-container">'+
+		'<button id="prev-button" class="paging-button" data-token="'+prevPageToken+'" data-query="'+q+'"' +
+		'onclick="prevPage();">Prev Page</button>' +
+		'<button id="next-button" class="paging-button" data-token="'+nextPageToken+'" data-query="'+q+'"' +
+		'onclick="nextPage();">Next Page</button></div>';	
+	}
+
+	return btnoutput;
 }
